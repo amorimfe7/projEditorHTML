7b6ecd248167862296d2cf6a138897c84a896d14
amorimfe7 <amorimfe7@gmail.com>
2024-01-23 12:22:31 -0300

    Desenhando a tela

--- Menu.cs
+++ Menu.cs
@@ -3,10 +3,40 @@ namespace EditorHTML{
 
         public static void Show(){
             Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Blue; //cor de fundo
+            Console.BackgroundColor = ConsoleColor.Green; //cor de fundo
             Console.ForegroundColor = ConsoleColor.Black; //cor da letra
 
+            Console.Clear();
+            DrawScreen();
+        }
 
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 38; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 38; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
         }
     }
 }
