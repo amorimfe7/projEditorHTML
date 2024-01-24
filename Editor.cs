c74fd4aaffd9fd6bd8f76989374c7871f9611e88
amorimfe7 <amorimfe7@gmail.com>
2024-01-24 14:52:09 -0300

    Criando Editor e função Show

--- Editor.cs
+++ Editor.cs
@@ -1,7 +1,16 @@
 using System.Text;
 
 namespace EditorHTML{
-    
-
-
+    public static class Editor {
+        public static void Show() {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("\n =========== MODO DE EDIÇÃO ===========\n");
+            Thread.Sleep(650);
+            Console.WriteLine("Digite seu texto abaixo || Pressione [ESC] para sair ");
+            // Start();
+        }
     }
+}
